3eb29639e6cc4f9026e36fa897216fd8b3b04c01
adam <adam@harpur.io>
2021-03-05 06:38:11 +0000

    chore: remove created hook

--- src/mixins/validation.js
+++ src/mixins/validation.js
@@ -1,4 +1,4 @@
-import * as validators from "@/utils/validators";
+import * as validators from "@/utils/validators"
 export default {
   computed: {
     isValid() {
@@ -9,20 +9,19 @@ export default {
                 typeof validator === "string" &&
                 typeof validators[validator] === "function"
               ) {
-                return validators[validator](this.value);
+                return validators[validator](this.value)
               }
               if (
                 Array.isArray(validator) &&
                 typeof validators[validator[0]] === "function"
               ) {
-                const [validatorName, argument] = validator;
-                return validators[validatorName](this.value, argument);
+                const [validatorName, argument] = validator
+                return validators[validatorName](this.value, argument)
               }
-              throw new Error(`validator '${validator}' not defined`);
+              throw new Error(`validator '${validator}' not defined`)
             })
             .every(Boolean)
-        : this.fieldValidators.length === 0;
-    }
+        : this.fieldValidators.length === 0
+    },
   },
-  created() {}
-};
+}
